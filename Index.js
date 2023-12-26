3c2b92a32112279578421d45b1899f81b1263871
wvjr <williamvazdireito@gmail.com>
2023-12-26 19:33:27 -0300

    projeto concluido

--- Index.js
+++ Index.js
@@ -1,3 +1,19 @@
-let nome = "felipe";
-console.log("oi");
+let Heroi = "Mario"
+const nomeHeroi = "O Herói de nome " 
+let expXP = 1.000
+let nivelHeroi = "Prata";
+
+switch(nivelHeroi){
+case "Ferro":
+console.log(nomeHeroi + Heroi + " está no nível de " + nivelHeroi);
+break;
+case "Bronze":
+console.log(nomeHeroi + Heroi + " está no nível de " + nivelHeroi);
+break;
+case "Prata":
+console.log(nomeHeroi + Heroi + " está no nível de " + nivelHeroi);
+break;
+default:
+console.log(nomeHeroi + Heroi + " está no nível de " + nivelHeroi);
+}
 
